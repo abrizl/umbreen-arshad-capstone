21e3128b153424ff639320880d0cb5aa30a3507c
abrizl <umbreenvirk@gmail.com>
2025-01-10 23:16:20 -0800

    add footer information

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './components/Header/Header';
 import LandingPage from './pages/LandingPage/LandingPage';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage/>}/>
         </Routes>
-        {/* <Footer/> */}
+        <Footer/>
     </BrowserRouter>
   )
 }
