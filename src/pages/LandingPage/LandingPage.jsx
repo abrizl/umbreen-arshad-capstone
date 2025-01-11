386c70943df59654d1c9fda7ffcc5d284a6cebb2
abrizl <umbreenvirk@gmail.com>
2025-01-10 21:51:22 -0800

    add landing page images

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,17 @@
 import './LandingPage.scss';
+import banner from '../../assets/images/Milk-Banner-Continuous-01.png';
+import banner2 from '../../assets/images/Milk-Banner-Continuous-02.png';
+
 
 function LandingPage() {
     return (
         <section className="landing">
             <div className="landing__hero">
-                Hello
+                <img src={banner} alt="milk pour on dark background" className="landing__image"/> 
+            </div>
+            <div className="landing__second">
+                <img src={banner2} alt="milk pour on dark background" className="landing__image"/>    
+
             </div>
         </section>
     )
