7204f46f3f4f016fb6233db11c13874121f5b2d4
Umbreen Arshad <umbreenvirk@gmail.com>
2025-01-12 15:55:44 -0800

    restructure jsx

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,56 +4,57 @@ import banner from '../../assets/images/Continuous Banner-01.png';
 
 function LandingPage() {
     return (
-        <section className="landing">
-            <div className="landing__container">
+
+        <section className="page">
+            
+            <section className="landing">
                 <img src={banner} alt="milk pour on dark background" className="landing__image"/> 
                 <div className="landing__overlay">
                     <p className="landing__heading">farm fresh</p>
                 </div>
-            </div>
+            </section>
 
-            <div className="landing__whySection">
-                <div className="landing__two">
-                    <div className="landing__item">
-                        <p className="landing__description">from the udders into the bottle</p>
+            <section className="why">
+                <div className="why__two">
+                    <div className="why__item">
+                        <p className="landing__description">from udders to bottle</p>
                     </div>
-                    <div className="landing__item">
-                        <p className="landing__topText">100%</p>
-                        <p className="landing__under">fresh, whole milk</p>
+                    <div className="why__item">
+                        <p className="why__topText">100%</p>
+                        <p className="why__under">fresh, whole milk</p>
                     </div>
                 </div>
 
-                <div className="landing__two">
-                    <div className="landing__item">
-                        <p className="landing__under">delivered</p>
-                        <p className="landing__topText">daily</p>
+                <div className="why__two">
+                    <div className="why__item">
+                        <p className="why__under">delivered twice</p>
+                        <p className="why__topText">daily</p>
                     </div>
-                    <div className="landing__item">
-                        <p className="landing__description">fresh from the farm, to your door</p>
-                    </div>
-                </div>
-
-                <div className="landing__two">
-                    <div className="landing__item">
-                        <p className="landing__description">we work with local farms to ensure quality and fair trade </p>
-                    </div>
-                    <div className="landing__item">
-                        <p className="landing__under">support</p>
-                        <p className="landing__topText">local</p>
+                    <div className="why__item">
+                        <p className="why__description">as per your needs and schedule</p>
                     </div>
                 </div>
 
-                <div className="landing__two">
-                    <div className="landing__item">
-                        <p className="landing__under">ditch the</p>
-                        <p className="landing__topText">plastic</p>
+                <div className="why__two">
+                    <div className="why__item">
+                        <p className="why__description">we work with local farms to ensure quality and fair trade </p>
                     </div>
-                    <div className="landing__item">
-                        <p className="landing__description">join our efforts to reduce single-use plastics </p>
+                    <div className="why__item">
+                        <p className="why__under">support</p>
+                        <p className="why__topText">local</p>
                     </div>
                 </div>
 
-            </div>
+                <div className="why__two">
+                    <div className="why__item">
+                        <p className="why__under">ditch the</p>
+                        <p className="why__topText">plastic</p>
+                    </div>
+                    <div className="why__item">
+                        <p className="why__description">join our efforts to reduce single-use plastics </p>
+                    </div>
+                </div>
+            </section>
             
         </section>
     )
