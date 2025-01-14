07c704271cd05c808fe094c471354b51dfbab586
Umbreen Arshad <umbreenvirk@gmail.com>
2025-01-13 17:03:59 -0800

    update about

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,16 +1,16 @@
+import './AboutPage.scss';
 
 function AboutPage() {
     return (
         <section className="about">
             <div className="about__area">
-                <h2 className="about__heading">our Story</h2>    
-                <p className="about__text">a simple idea born from necessity. From reading about </p>    
+                <h2 className="about__name">milkotov</h2>    
+                <p className="about__subtitle">born from necessity </p>    
 
             </div>
 
-            <div className="about__area">
-                <h2 className="about__heading">why milkotov</h2>
-                <p className="about__text">a bottle of milkotov presents the same symbol of rebellion against contaminated milk as the molotov cocktails used throughout history to ignite change in their respective communities.</p>    
+            <div className="about__why">
+                <p className="about__text">each bottle of milkotov is a bold symbol of rebellion against contaminated milk -- much like the molotov cocktails used throughout history to rebel against oppression.</p>    
                 <p className="about__text">we promise uncontaminated milk that can be consumed without fear. Suitable for your little ones switching to whole milk from formula or your teens entering their growing years. </p>    
 
             </div>
