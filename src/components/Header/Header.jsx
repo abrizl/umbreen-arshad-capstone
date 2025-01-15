2260f6008f1c443e0add76a404d6e954fcec348d
Umbreen Arshad <umbreenvirk@gmail.com>
2025-01-15 13:05:12 -0800

    update header and sign-in bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,15 @@ import {Link} from 'react-router-dom';
 
 function Header() {
     return (
-        <section className="header">
-            <img src={milkotovLogo} alt="Milkotov logo" className="header__logo"/>
-            <div className="header__navigation">
-                <Link to= "/" className="header__link">Home</Link>
-                <Link to= "/about" className="header__link">About</Link>
-                <Link to= "/contact" className="header__link">Contact</Link>
-                <button className="header__button">Schedule a Delivery</button>
+        <section className="headerContainer">
+            <div className="header">
+                <img src={milkotovLogo} alt="Milkotov logo" className="header__logo"/>
+                <div className="header__navigation">
+                    <Link to= "/" className="header__link">Home</Link>
+                    <Link to= "/about" className="header__link">About</Link>
+                    <Link to= "/contact" className="header__link">Contact</Link>
+                    <button className="header__button">Schedule a Delivery</button>
+                </div>
             </div>
         </section>
     )
