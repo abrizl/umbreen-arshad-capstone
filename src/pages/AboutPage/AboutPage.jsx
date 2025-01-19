ac391471413a0cef27879833964fd5d833b9a93b
Umbreen Arshad <umbreenvirk@gmail.com>
2025-01-19 14:55:47 -0800

    make about-page responsive

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -5,23 +5,25 @@ import milkotovIcon from "../../assets/icons/milkotovIcon-01.svg";
 function AboutPage() {
     return (
         <section className="about">
-            <div className="about__area">
-                <h2 className="about__name">milkotov</h2> 
-                <h4 className="about__pronounciation">ˈmɪl.koʊ.tɔv</h4> 
-            </div>
+            <div className="about__container">
+                <div className="about__area">
+                    <h2 className="about__name">milkotov</h2> 
+                    <h4 className="about__pronounciation">ˈmɪl.koʊ.tɔv</h4> 
+                </div>
 
-            <p className="about__subtitle">born from necessity </p>    
+                <p className="about__subtitle">born from necessity </p>    
 
-            <div className="about__why">
-                <p className="about__text">each bottle of milkotov is a bold symbol of rebellion against contaminated milk -- much like the molotov cocktails used throughout history to rebel against oppression.</p>    
-                <p className="about__text">we promise uncontaminated milk ready for consumption without fear. Suitable for everyone; your little ones switching to whole milk or your teens entering their growing years. </p>    
+                <div className="about__why">
+                    <p className="about__text">each bottle of milkotov is a bold symbol of rebellion against contaminated milk -- much like the molotov cocktails used throughout history to rebel against oppression.</p>    
+                    <p className="about__text">we promise uncontaminated milk ready for consumption without fear. Suitable for everyone; your little ones switching to whole milk or your teens entering their growing years. </p>    
+                </div>
             </div>
 
             <div className="about__container">
                 <div className="about__mission">
                     <img src={milkotovIcon} alt="milkotov icon" className='about__image'/>
                     <h3 className="about__subheading">Quality Assurance & Sourcing</h3>
-                    <p className="about__detail">We source our milk from our own local farm. Our buffaloes are grass-fed and never given hormones, antibiotics, or injections for milk production.</p>
+                    <p className="about__detail">We source our milk from our local farm. Our buffaloes are grass-fed and never given hormones, antibiotics, or injections for milk production.</p>
                 </div>
 
                 <div className="about__mission">
